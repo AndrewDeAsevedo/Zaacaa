4ca20efeb753c36013e8d6e9f7ea7a17c3d1eb1e
Andrew De Asevedo <andrewdeasevedo11@gmail.com>
2024-02-03 12:51:54 -0500

    Fixed the logo, adjusted the login button

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,12 +4,12 @@ import "./header.css";
 export default function Header() {
   return (
     <header className="header">
-      <img src={require("./favicon.png")} className="Logo" alt="logo" />
+      <img src={require("./favicon.png")} className="App-logo" alt="logo" />
       <nav className="Nav">
         <a href="/home">Home</a>
         <a href="/exams">Exams</a>
         <a href="/admin">Admin</a>
-        <a href="/login">Login</a>
+        <b href="/login">Log-in</b>
       </nav>
     </header>
   );
