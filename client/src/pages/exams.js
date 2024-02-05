736d1b45effc2d9c6b9ebbcc0a67382ce446d5b1
Andrew De Asevedo <andrewdeasevedo11@gmail.com>
2024-02-04 23:15:41 -0500

    Started the Exams page: added table with mild data

--- client/src/pages/exams.js
+++ client/src/pages/exams.js
@@ -1,10 +1,38 @@
+import React from "react";
 import Header from "../components/header";
+import "./exams.css";
+
+// Temporary trash data, later we are gonna replace this with
+// what we get from their API
+const data = [
+  { name: "Anom", age: 19, gender: "Male" },
+  { name: "Megha", age: 19, gender: "Female" },
+  { name: "Subham", age: 25, gender: "Male" },
+];
 
 export default function Exams() {
   return (
     <>
       <Header />
-      <h2> Exams Page</h2>
+      <h2> Exams Page </h2>
+      <div className="Exams">
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+          </tr>
+          {data.map((val, key) => {
+            return (
+              <tr key={key}>
+                <td>{val.name}</td>
+                <td>{val.age}</td>
+                <td>{val.gender}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
     </>
   );
 }
