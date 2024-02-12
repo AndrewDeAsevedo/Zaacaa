5ce06a3f884ad49fc659c420fae9e0df3606e9a0
Andrew De Asevedo <andrewdeasevedo11@gmail.com>
2024-02-12 13:21:16 -0500

    fixed some bugs that were caused from the merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
+import "./App.css";
 
-import { useApi } from './hooks/use-api';
+import { useApi } from "./hooks/use-api";
 
 // BrowserRouter wraps everywhere we want to use the router, Router wraps all individual routes, Route creates a single route
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 // Pages and components
-import Home from './pages/Home'
-import Admin from './pages/Admin'
+import Home from "./pages/home";
+import Admin from "./pages/admin";
 import Exams from "./pages/exams";
-import CreateExam from './pages/CreateExam'
-import Patient from './pages/Patient'
+import CreateExam from "./pages/CreateExam";
+import Patient from "./pages/patient";
 import Login from "./pages/login";
 //import Navbar from './components/Navbar'
 
@@ -27,33 +27,15 @@ function App() {
       </header>
   */}
       <BrowserRouter>
-        <div className="pages"> 
+        <div className="pages">
           <Routes>
             <Route index element={<Login />} />
-            <Route 
-              path="/login" 
-              element={<Login />} 
-            />
-            <Route 
-              path="/"
-              element={<Home />}
-            />
-            <Route  
-              path="/admin"
-              element={<Admin />}
-            />
-            <Route 
-              path="/exams" 
-              element={<Exams />} 
-            />
-            <Route 
-              path="/exam/create"
-              element={<CreateExam />}
-            />
-            <Route 
-              path="/patient"
-              element={<Patient />}
-            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/exams" element={<Exams />} />
+            <Route path="/exam/create" element={<CreateExam />} />
+            <Route path="/patient" element={<Patient />} />
           </Routes>
         </div>
       </BrowserRouter>
