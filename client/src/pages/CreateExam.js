222b8acc5ed3fa26467112f6e2d88dc071166051
Andrew De Asevedo <andrewdeasevedo11@gmail.com>
2024-02-26 01:15:24 -0500

    Fixed URL bug. Forums can no longer be submitted empty.

--- client/src/pages/CreateExam.js
+++ client/src/pages/CreateExam.js
@@ -12,12 +12,29 @@ const CreateExam = () => {
   const [successMessage, setSuccessMessage] = useState(""); // State to control success message display
   let navigate = useNavigate();
 
-  const onSubmit = async () => {
+  const onSubmit = async (e) => {
     try {
       //const response =  useApi('/admin/create/{"age": "60","sex": "F","bmi": "27.6","exams":[{"examId":"exam_1","findings":"clear lungs"},{"examId":"exam_1","findings":"swollen lungs"}}')
       const response = await fetch(
         "http://localhost:9000/admin/create/" + JSON.stringify(inputs)
       );
+      // prevent empty forum submission
+      if (
+        !inputs.patientID ||
+        !inputs.age ||
+        !inputs.sex ||
+        !inputs.bmi ||
+        !inputs.zipcode ||
+        !inputs.examID ||
+        !inputs.date ||
+        !inputs.keyFindings ||
+        !inputs.brixiaScores ||
+        !inputs.imageURL
+      ) {
+        // If any required field is empty, display an error message and return early
+        setSuccessMessage("Please fill out all fields.");
+        return;
+      }
 
       // Handle success
       if (response.ok) {
@@ -50,125 +67,143 @@ const CreateExam = () => {
     navigate("/admin"); // Example: navigating back to the homepage
   };
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({...values, [name]: value}))
-    }
-    const handleURI = (event) => {
-        const name = event.target.name;
-        const value = encodeURI(event.target.value);
-        setInputs(values => ({...values, [name]: value}))
-    }
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+  const handleURI = (event) => {
+    const name = event.target.name;
+    const value = encodeURIComponent(event.target.value);
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
-    return (
-        <><Header /><div className="createExam">
-            <style>
-                @import
-                url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap')
-            </style>
-            <h2>Create Exam</h2>
-            <div class="center-container">
-            <form className="form-container">
-                <div className="form-item">
-                    <label htmlFor="patientID">Patient ID:</label>
-                    <input
-                        type="text"
-                        name="patientID"
-                        id="patientID"
-                        value={inputs.patientID || ""}
-                        onChange={handleChange} />
-                </div>
-                <div className="form-item">
-                    <label htmlFor="age">Age:</label>
-                    <input
-                        type="number"
-                        name="age"
-                        id="age"
-                        value={inputs.age || ""}
-                        onChange={handleChange} />
-                </div>
-                <div className="form-item">
-                    <label htmlFor="sex">Sex:</label>
-                    <input
-                        type="text"
-                        name="sex"
-                        id="sex"
-                        value={inputs.sex || ""}
-                        onChange={handleChange} />
-                </div>
-                <div className="form-item">
-                    <label htmlFor="bmi">BMI:</label>
-                    <input
-                        type="number"
-                        name="bmi"
-                        id="bmi"
-                        value={inputs.bmi || ""}
-                        onChange={handleChange} />
-                </div>
-                <div className="form-item">
-                    <label htmlFor="zipcode">Zip Code:</label>
-                    <input
-                        type="number"
-                        name="zipcode"
-                        id="zipcode"
-                        value={inputs.zipcode || ""}
-                        onChange={handleChange} />
-                </div>
-                <div className="form-item">
-                    <label htmlFor="examID">Exam ID:</label>
-                    <input
-                        type="text"
-                        name="examID"
-                        id="examID"
-                        value={inputs.examID || ""}
-                        onChange={handleChange} />
-                </div>
-                <div className="form-item">
-                    <label htmlFor="date">Date:</label>
-                    <input
-                        type="date"
-                        name="date"
-                        id="date"
-                        value={inputs.date || ""}
-                        onChange={handleChange} />
-                </div>
-                <div className="form-item">
-                    <label htmlFor="keyFindings">Key Findings:</label>
-                    <input
-                        type="text"
-                        name="keyFindings"
-                        id="keyFindings"
-                        value={inputs.keyFindings || ""}
-                        onChange={handleChange} />
-                </div>
-                <div className="form-item">
-                    <label htmlFor="brixiaScores">Brixia Scores:</label>
-                    <input
-                        type="text"
-                        name="brixiaScores"
-                        id="brixiaScores"
-                        value={inputs.brixiaScores || ""}
-                        onChange={handleChange} />
-                </div>
-                <div className="form-item">
-                    <label htmlFor="imageurl">Image URL:</label>
-                    <input
-                        type="url"
-                        name="imageURL"
-                        id="imageURL"
-                        value={inputs.imageURL || ""}
-                        onChange={handleURI} />
-                </div>
-                {/* Button is placed outside the form to align it at the bottom of the page */}
-            </form></div>
-            <div className="button-container">
-                <button type="button" className="submit-button" onClick={onSubmit}>Add Exam</button>
-                <button type="button" className="cancel-button" onClick={onCancel}>Cancel</button>
+  return (
+    <>
+      <Header />
+      <div className="createExam">
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap')
+        </style>
+        <h2>Create Exam</h2>
+        <div class="center-container">
+          <form className="form-container">
+            <div className="form-item">
+              <label htmlFor="patientID">Patient ID:</label>
+              <input
+                type="text"
+                name="patientID"
+                id="patientID"
+                value={inputs.patientID || ""}
+                onChange={handleChange}
+              />
             </div>
-            {successMessage && <p>{successMessage}</p>}
-        </div></>
-    );
-    }
+            <div className="form-item">
+              <label htmlFor="age">Age:</label>
+              <input
+                type="number"
+                name="age"
+                id="age"
+                value={inputs.age || ""}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-item">
+              <label htmlFor="sex">Sex:</label>
+              <input
+                type="text"
+                name="sex"
+                id="sex"
+                value={inputs.sex || ""}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-item">
+              <label htmlFor="bmi">BMI:</label>
+              <input
+                type="number"
+                name="bmi"
+                id="bmi"
+                value={inputs.bmi || ""}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-item">
+              <label htmlFor="zipcode">Zip Code:</label>
+              <input
+                type="number"
+                name="zipcode"
+                id="zipcode"
+                value={inputs.zipcode || ""}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-item">
+              <label htmlFor="examID">Exam ID:</label>
+              <input
+                type="text"
+                name="examID"
+                id="examID"
+                value={inputs.examID || ""}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-item">
+              <label htmlFor="date">Date:</label>
+              <input
+                type="date"
+                name="date"
+                id="date"
+                value={inputs.date || ""}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-item">
+              <label htmlFor="keyFindings">Key Findings:</label>
+              <input
+                type="text"
+                name="keyFindings"
+                id="keyFindings"
+                value={inputs.keyFindings || ""}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-item">
+              <label htmlFor="brixiaScores">Brixia Scores:</label>
+              <input
+                type="text"
+                name="brixiaScores"
+                id="brixiaScores"
+                value={inputs.brixiaScores || ""}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-item">
+              <label htmlFor="imageurl">Image URL:</label>
+              <input
+                type="url"
+                name="imageURL"
+                id="imageURL"
+                value={inputs.imageURL || ""}
+                onChange={handleURI}
+              />
+            </div>
+            {/* Button is placed outside the form to align it at the bottom of the page */}
+          </form>
+        </div>
+        <div className="button-container">
+          <button type="button" className="submit-button" onClick={onSubmit}>
+            Add Exam
+          </button>
+          <button type="button" className="cancel-button" onClick={onCancel}>
+            Cancel
+          </button>
+        </div>
+        {successMessage && <p>{successMessage}</p>}
+      </div>
+    </>
+  );
+};
 
-export default CreateExam
+export default CreateExam;
